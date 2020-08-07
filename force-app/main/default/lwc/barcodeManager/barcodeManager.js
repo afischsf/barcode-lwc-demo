083bb9aaa8296371fbd62184027b376391438b35
Adam Fisch <afisch@salesforce.com>
2020-08-07 12:33:25 -0600

    single and multi-scan done

--- force-app/main/default/lwc/barcodeManager/barcodeManager.js
+++ force-app/main/default/lwc/barcodeManager/barcodeManager.js
@@ -1,13 +1,18 @@
 import { LightningElement, track } from 'lwc';
 import scannerPicker from './scanner-picker.html';
-import singleScan from './barcodeManager.html';
+import barcodeListPage from './barcodeManager.html';
 import detailPage from './detail.html';
 import { getBarcodeScanner } from 'lightning/mobileCapabilities';
 
+import resources from '@salesforce/resourceUrl/barcodeScanner';
+
 export default class NimbusBarcodeScanner extends LightningElement {
+
+    splashLogo = `${resources}/barcodeScannerResource/splashScreen.svg`;
 
     barcodeScanner;
     screenNumber = "1";
+    selectedTab = "multi";
 
     detailType;
     @track detail;
@@ -23,17 +28,9 @@ export default class NimbusBarcodeScanner extends LightningElement {
             case "1":
                 return scannerPicker;
             case "2":
-                return singleScan;
+                return barcodeListPage;
             case "4":
                 return detailPage;
-        }
-    }
-
-    switchTemplate(e) {
-        this.screenNumber = e.target.value;
-
-        if (e.target.name === "single") {
-            this.openSingleScan();
         }
     }
 
@@ -42,6 +39,7 @@ export default class NimbusBarcodeScanner extends LightningElement {
     }
 
     openSingleScan() {
+        this.selectedTab = "single";
         if (this.barcodeScanner.isAvailable()) {
             this.barcodeScanner.beginCapture(
                 { barcodeTypes: [] }
@@ -73,29 +71,97 @@ export default class NimbusBarcodeScanner extends LightningElement {
         }
     }
 
+    openMultiScan() {
+        this.selectedTab = "multi";
+        if (this.barcodeScanner.isAvailable()) {
+            this.barcodeScanner.beginCapture(
+                { barcodeTypes: [] }
+            )
+            .then((barcode) => this.addMultiScannedBarcode(barcode))
+            .catch(error => {
+                this.error = error;
+                this.barcodeScanner.endCapture();
+            })
+            .finally(() => this.continueMultiScan());
+        }
+    }
+
+    continueMultiScan() {
+        if (this.barcodeScanner.isAvailable()) {
+            setTimeout(() => {
+                this.barcodeScanner.resumeCapture()
+                .then((barcode) => this.addMultiScannedBarcode(barcode))
+                .catch((error) => {
+                    this.error = error;
+                    this.barcodeScanner.endCapture();
+                })
+                .finally(() => this.continueMultiScan());
+            }, 1000);
+        }
+    }
+
+    addMultiScannedBarcode(barcode) {
+        const last = this.last(this.multipleScanRecords)
+        let count = 0;
+        if (last !== undefined) {
+            count = last.id;
+        }
+        count++;
+
+        this.multipleScanRecords.push({
+            id: count,
+            name: "",
+            description: "",
+            quantity: 1,
+            barcode: barcode.value
+        });
+
+        this.detail = Object.assign({}, this.multipleScanRecords[this.multipleScanRecords.length - 1]);
+        this.detailType = "multi";
+        this.screenNumber = "2";
+    }
+
     viewDetail(event) {
-        const id = event.currentTarget.dataset.id;
-        this.detailType = event.currentTarget.dataset.type;
+        const id = event.detail.itemId;
+        this.detailType = event.detail.scanType;
         let detail;
         if (this.detailType === "single") {
             detail = this.singleScanRecords.find(value => value.id  == id);
+        } else if (this.detailType === "multi") {
+            detail = this.multipleScanRecords.find(value => value.id  == id);
         }
         this.detail = Object.assign({}, detail);
         this.screenNumber = "4";
     }
 
-    backButtonPressed() {
-        if (this.detailType === "single") {
-            this.screenNumber = "2";
+    deleteDetail(event) {
+        const id = event.detail.itemId;
+        if (event.detail.scanType === "single") {
+            const index = this.singleScanRecords.findIndex(item => item.id == id);
+            if (index > -1) {
+                this.singleScanRecords.splice(index, 1);
+            }
+        } else if (event.detail.scanType === "multi") {
+            const index = this.multipleScanRecords.findIndex(item => item.id == id);
+            if (index > -1) {
+                this.multipleScanRecords.splice(index, 1);
+            }
         }
+    }
+
+    backButtonPressed() {
+        this.screenNumber = "2";
     }
 
     savePressed() {
         if (this.detailType === "single") {
             const index = this.singleScanRecords.findIndex(x => x.id == this.detail.id);
             this.singleScanRecords[index] = this.detail;
-            this.screenNumber = "2";
+        } else if (this.detailType === "multi") {
+            const index = this.multipleScanRecords.findIndex(x => x.id == this.detail.id);
+            this.multipleScanRecords[index] = this.detail;
         }
+        this.screenNumber = "2";
     }
 
     textInputChange(event) {
--- force-app/main/default/lwc/scanListItem/scanListItem.js
+++ force-app/main/default/lwc/scanListItem/scanListItem.js
@@ -1,6 +1,40 @@
 import { LightningElement, api } from "lwc";
 
+const
+    EVENT_DELETE_ITEM_CLICK = "deleteitemclick",
+    EVENT_ITEM_CLICK = "itemclick";
+
 export default class ScanListItem extends LightningElement {
     @api item;
     @api isFirstItem;
+    @api scanType;
+
+    deleteItem(event) {
+        event.stopPropagation();
+        this.dispatchEvent(
+            new CustomEvent(
+                EVENT_DELETE_ITEM_CLICK,
+                { 
+                    detail: {
+                        scanType: this.scanType,
+                        itemId: this.item.id
+                    }
+                }
+            )
+        )
+    }
+
+    cellTapped(event) {
+        this.dispatchEvent(
+            new CustomEvent(
+                EVENT_ITEM_CLICK,
+                { 
+                    detail: {
+                        scanType: this.scanType,
+                        itemId: this.item.id
+                    }
+                }
+            )
+        )
+    }
 }
